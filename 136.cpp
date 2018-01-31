b4f6ca28ed98bd8f2f4a69a26d952c59262e8495
Jimmy <abc8755987@gmail.com>
2018-01-31 14:42:23 +0800

    Create 136.cpp

---
+++ 136.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size(); i+=2)
+            if(nums[i] != nums[i+1]) return nums[i];
+    }
+};
