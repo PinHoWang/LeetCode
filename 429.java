49d98a5ac109316fb579f0030c341bff94d77239
Jimmy <abc8755987@gmail.com>
2018-08-09 14:58:21 +0800

    Two solution: BFS & Recursion
    This recursion solution using the whole children pass to the function.

--- 429.java
+++ 429.java
@@ -14,26 +14,58 @@ class Node {
 */
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
-        if(root == null) return new LinkedList<>();
+        /* Level-order (BFS) */
+//         if(root == null) return new LinkedList<>();
         
-        List<List<Integer>> ls = new LinkedList<>();
-        Queue<Node> q = new LinkedList<>();
-        //ls.add(new LinkedList<Integer>(root.val));
-        q.add(root);
+//         List<List<Integer>> ls = new LinkedList<>();
+//         Queue<Node> q = new LinkedList<>();
+//         //ls.add(new LinkedList<Integer>(root.val));
+//         q.add(root);
         
-        while(!q.isEmpty()) {
-            int qNum = q.size();
-            List<Integer> level = new LinkedList<>();
-            for(int i = 0; i < qNum; i++) {
-                Node n = q.poll();
-                level.add(n.val);
-                for(Node nn : n.children) {
-                    q.add(nn);
-                }
-            }
-            ls.add(level);
+//         while(!q.isEmpty()) {
+//             int qNum = q.size();
+//             List<Integer> level = new LinkedList<>();
+//             for(int i = 0; i < qNum; i++) {
+//                 Node n = q.poll();
+//                 level.add(n.val);
+//                 for(Node nn : n.children) {
+//                     q.add(nn);
+//                 }
+//             }
+//             ls.add(level);
+//         }
+        
+//         return ls;
+        
+        
+        /* Recursion */
+        if(root == null){
+            return new ArrayList<List<Integer>>();
         }
         
-        return ls;
+        List<Node> in = new ArrayList<Node>();
+        in.add(root);
+        
+        return levelHelper(in);
+    }
+    
+    private List<List<Integer>> levelHelper(List<Node> levelNodes){
+        if(levelNodes.isEmpty()){
+            return new ArrayList<List<Integer>>();
+        }
+
+        List<Integer> currLevel = new ArrayList<Integer>();
+        List<Node> nextLevel = new ArrayList<Node>();
+        
+        for(Node n : levelNodes){
+            currLevel.add(n.val);
+            nextLevel.addAll(n.children);
+        }
+        
+        List<List<Integer>> ret = levelHelper(nextLevel);
+        ret.add(0, currLevel);
+        
+        return ret;
+        
     }
 }
