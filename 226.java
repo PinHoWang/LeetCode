a0db4e3d14974ec828f5a7bf9958d49a2db8aede
Jimmy <abc8755987@gmail.com>
2018-08-10 09:51:04 +0800

    Invert Tree

--- 226.java
+++ 226.java
@@ -9,33 +9,42 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null) return null;
+        /* BFS */
+//         if(root == null) return null;
         
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()) {
-            int qNum = q.size();
-            for(int i = 0; i < qNum; i++) {
-                TreeNode n = q.poll();
-                if(n.left != null && n.right != null) {
-                    q.add(n.left);
-                    q.add(n.right);
-                    TreeNode l = n.left;
-                    n.left = n.right;
-                    n.right = l;
-                }
-                else if(n.left != null && n.right == null) {
-                    q.add(n.left);
-                    n.right = n.left;
-                    n.left = null;
-                }
-                else if(n.left == null && n.right != null) {
-                    q.add(n.right);
-                    n.left = n.right;
-                    n.right = null;
-                }
-            }
-        }
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(root);
+//         while(!q.isEmpty()) {
+//             int qNum = q.size();
+//             for(int i = 0; i < qNum; i++) {
+//                 TreeNode n = q.poll();
+//                 if(n.left != null && n.right != null) {
+//                     q.add(n.left);
+//                     q.add(n.right);
+//                     TreeNode l = n.left;
+//                     n.left = n.right;
+//                     n.right = l;
+//                 }
+//                 else if(n.left != null && n.right == null) {
+//                     q.add(n.left);
+//                     n.right = n.left;
+//                     n.left = null;
+//                 }
+//                 else if(n.left == null && n.right != null) {
+//                     q.add(n.right);
+//                     n.left = n.right;
+//                     n.right = null;
+//                 }
+//             }
+//         }
+//         return root;
+        
+        /* Recursion */
+        if(root == null) return null;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
         return root;
     }
 }
