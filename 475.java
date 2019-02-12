687f74cee2dc267be588bbf40a29a130be7f0abb
Jimmy <abc8755987@gmail.com>
2019-02-12 13:18:22 -0500

    Update to O(NlgN)?

--- 475.java
+++ 475.java
@@ -1,25 +1,39 @@
 class Solution {
     public int findRadius(int[] houses, int[] heaters) {
         /* Brute Force Solution */
-        int[] cover = new int[houses.length];
-        for(int i = 0; i < cover.length; i++) cover[i] = Integer.MAX_VALUE;
+//         int[] cover = new int[houses.length];
+//         for(int i = 0; i < cover.length; i++) cover[i] = Integer.MAX_VALUE;
         
-        for(int heatPos = 0; heatPos < heaters.length; heatPos++)
+//         for(int heatPos = 0; heatPos < heaters.length; heatPos++)
+//         {
+//             for(int housPos = 0; housPos < houses.length; housPos++)
+//             {
+//                 if(Math.abs(houses[housPos] - heaters[heatPos]) < cover[housPos])
+//                 {
+//                     cover[housPos] = Math.abs(houses[housPos] - heaters[heatPos]);
+//                 }
+//             }
+//         }
+        
+//         int max = Integer.MIN_VALUE;
+//         for(int j = 0; j < cover.length; j++)
+//         {
+//             if(cover[j] > max) max = cover[j];
+//         }
+//         return max;
+        
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        
+        int i = 0, result = 0;
+        for(int house : houses)
         {
-            for(int housPos = 0; housPos < houses.length; housPos++)
+            while(i < heaters.length-1 && (Math.abs(heaters[i]-house) >= Math.abs(heaters[i+1] - house)))
             {
-                if(Math.abs(houses[housPos] - heaters[heatPos]) < cover[housPos])
-                {
-                    cover[housPos] = Math.abs(houses[housPos] - heaters[heatPos]);
-                }
+                i++;
             }
+            result = Math.max(result, Math.abs(heaters[i] - house));
         }
-        
-        int max = Integer.MIN_VALUE;
-        for(int j = 0; j < cover.length; j++)
-        {
-            if(cover[j] > max) max = cover[j];
-        }
-        return max;
+        return result;
     }
 }
