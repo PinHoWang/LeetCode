edf6290c815ceffb5fe5250d65f83d9fdfccddf3
Jimmy <abc8755987@gmail.com>
2019-02-16 12:25:42 -0500

    Update recursion solution

--- 206.java
+++ 206.java
@@ -8,23 +8,34 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head == null) return null;
-        if(head.next == null) return head;
+        /* Three Pointer */
+//         if(head == null) return null;
+//         if(head.next == null) return head;
         
-        ListNode temp1 = head;
-        ListNode temp2 = temp1.next;
-        ListNode temp3 = temp2.next;
+//         ListNode temp1 = head;
+//         ListNode temp2 = temp1.next;
+//         ListNode temp3 = temp2.next;
         
-        while(true) {
-            temp2.next = temp1;
-            temp1 = temp2;
-            temp2 = temp3;
-            if(temp3 == null) { break; }
-            temp3 = temp3.next;
-        }
+//         while(true) {
+//             temp2.next = temp1;
+//             temp1 = temp2;
+//             temp2 = temp3;
+//             if(temp3 == null) { break; }
+//             temp3 = temp3.next;
+//         }
         
+//         head.next = null;
+//         head = temp1;
+//         return head;
+        
+        /* Recursion */
+        if(head == null || head.next == null) return head;
+        
+        ListNode nextNode = head.next;
+        ListNode newHead = reverseList(nextNode);
+        nextNode.next = head;
         head.next = null;
-        head = temp1;
-        return head;
+        return newHead;
+        
     }
 }
