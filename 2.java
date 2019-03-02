dbae883a4122aea1d8345d7b8739f06ebfd4778e
Jimmy <abc8755987@gmail.com>
2019-03-02 11:43:29 -0500

    Update 2.java

--- 2.java
+++ 2.java
@@ -8,37 +8,29 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode prev = new ListNode(0);
-        ListNode head = prev;
-        int carry = 0;
-        while (l1 != null || l2 != null || carry != 0) {
-            ListNode cur = new ListNode(0);
-            int sum = ((l2 == null) ? 0 : l2.val) + ((l1 == null) ? 0 : l1.val) + carry;
-            cur.val = sum % 10;
-            carry = sum / 10;
-            prev.next = cur;
-            prev = cur;
-            
-            l1 = (l1 == null) ? l1 : l1.next;
-            l2 = (l2 == null) ? l2 : l2.next;
-        }
-        return head.next;
-    }
-    
-    private ListNode reverse(ListNode n)
-    {
-        ListNode prev = null;
-        ListNode cur = n;
-        ListNode back = n.next;
-        
-        while(cur != null)
+        ListNode result = new ListNode(0);
+        ListNode cur = result;
+        int ten = 0;
+        while(l1 != null || l2 != null || ten != 0)
         {
-            cur.next = prev;
-            prev = cur;
-            cur = back;
-            if(cur == null) break;
-            back = back.next;
+            int i = 0;
+            if(l1 != null)
+            {
+                i += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null)
+            {
+                i += l2.val;
+                l2 = l2.next;
+            }
+            
+            ten = (i + cur.val)/10;
+            cur.val = (i + cur.val) % 10;
+            if(l1 != null || l2 != null || ten != 0) cur.next = new ListNode(ten);
+            cur = cur.next;
         }
-        return prev;
+        
+        return result;
     }
 }
