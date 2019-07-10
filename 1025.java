dc8904f250f7ebca82842135469b4c763585ca52
Jimmy <abc8755987@gmail.com>
2019-07-10 15:36:27 +0800

    Create 1025.java

---
+++ 1025.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean divisorGame(int N) {
+        if ( N == 1){
+            return false;
+        }
+        else {
+            return !divisorGame(N - 1);
+        }
+    }
+}
