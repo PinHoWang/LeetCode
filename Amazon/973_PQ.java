ab4aed33a1f1276a674be2ceb2f1eb2c987f5733
Jimmy <abc8755987@gmail.com>
2019-10-02 14:04:51 -0400

    Update 973_PQ.java

--- Amazon/973_PQ.java
+++ Amazon/973_PQ.java
@@ -1,2 +1,15 @@
-
-// TODO
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(K, (a, b) -> {
+            return a[0]*a[0] + a[1]*a[1] - b[0]*b[0] - b[1]*b[1];
+        });
+        
+        for(int[] nums : points) pq.add(nums);
+        
+        int[][] result = new int[K][2];
+        for(int i = 0; i < result.length; i++) result[i] = pq.poll();
+        
+        return result;
+        
+    }
+}
