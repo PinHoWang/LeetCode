89183d61c8ed782a137b59a184ce5056648134c2
Jimmy <abc8755987@gmail.com>
2019-10-21 16:49:07 -0400

    Update 2.java

--- Amazon/2.java
+++ Amazon/2.java
@@ -8,50 +8,42 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // Using the extra variable to record the second digit
-        ListNode head = new ListNode(0);
-        ListNode cur = head;
-        int temp = 0;
+        if(l1 == null && l2 == null) return null;
+        else if(l1 == null && l2 != null) return l2;
+        else if(l1 != null && l2 == null) return l1;
+        
+        ListNode preHead = new ListNode(0);
+        ListNode cur = preHead;
+        int x = 0;
         while(l1 != null || l2 != null) {
             if(l1 == null) {
-                int v = l2.val + temp;
-                cur.next = new ListNode(v % 10);
+                int rem = (l2.val + x) % 10;
+                x = (l2.val + x) / 10;
+                cur.next = new ListNode(rem);
                 cur = cur.next;
-                temp =  v / 10;
                 l2 = l2.next;
             }
             else if(l2 == null) {
-                int v = l1.val + temp;
-                cur.next = new ListNode(v % 10);
+                int rem = (l1.val + x) % 10;
+                x = (l1.val + x) / 10;
+                cur.next = new ListNode(rem);
                 cur = cur.next;
-                temp = v / 10;
                 l1 = l1.next;
             }
             else {
-                // l1.next && l2.next are not null
-                int v = l1.val + l2.val + temp;
-                cur.next = new ListNode(v % 10);
+            
+                int rem = (l1.val + l2.val + x) % 10;
+                cur.next = new ListNode(rem);
                 cur = cur.next;
-                temp = v / 10;
+            
+                x = (l1.val + l2.val + x) / 10;
                 l1 = l1.next;
                 l2 = l2.next;
             }
         }
         
-        if(temp != 0) {
-            if(temp / 10 == 0) cur.next = new ListNode(temp % 10);
-            else {
-                cur.next = new ListNode(temp % 10);
-                cur.next.next = new ListNode(temp / 10);
-            }
-        }
-        // int digit = 0;
-        // ListNode ptr = head.next;
-        // int result = 0;
-        // while(ptr != null) {
-        //     result += (ptr.val * 10^digit);
-        //     digit++;
-        // }
-        return head.next;
+        if(x != 0) cur.next = new ListNode(x);
+        
+        return preHead.next;
     }
 }
